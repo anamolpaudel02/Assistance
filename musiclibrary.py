d0d202af45884803c0180fc36da254311add7724
anamolpaudel02 <anamolpaudel123@gmail.com>
2026-05-11 18:00:32 +0545

    this is an music link which i love

---
+++ musiclibrary.py
@@ -0,0 +1,9 @@
+music = {
+    "play music": "https://www.youtube.com/watch?v=bU2EvRBUmxc&list=PLdpPOKqVV3VRwlUOWxD-xW4CLFqPEykjF",
+    "play waka waka":"https://youtu.be/pRpeEdMmmQ0?si=usx2zVC2eHplCvE6",
+    "play shape of you":"https://youtu.be/JGwWNGJdvx8?si=9n2sXo7l3m1j5a9e",
+    "play yellow":"https://youtu.be/yKNxeF4KMsY?si=_sVieJEOhD5IN3T-"
+}
+
+
+
